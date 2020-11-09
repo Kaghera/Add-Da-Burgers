20e25052b91c9523ef6a2153bc8fa25b589a09c9
Kashyap Aghera <kaagh.10@gmail.com>
2020-11-09 16:15:39 -0500

    modified files

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -15,16 +15,7 @@ router.get("/index", function(req, res) {
       };
       res.render('index', handlebarsObject);
     });
-// Index Page 
-// router.get('/index', function (req, res) 
-// {
-//   burger.selectAll(function(data) 
-//   {
-//     var hbsObject = { burgers: data };
-//     //console.log(hbsObject);
-//     res.render('index', hbsObject);
-//   });
-// });
+
 
 });
 
@@ -50,62 +41,3 @@ router.put("/api/burger/:id", function(req, res) {
 
 module.exports = router;
 
-// var express = require('express');
-// var router = express.Router();
-// var burger = require('../models/burgerModel');
-
-
-// //Setup Routes
-
-// // Index Redirect
-// router.get('/', function (req, res) 
-// {
-//   res.redirect('/index');
-// });
-// // router.get("/", function(req, res) {
-// //     burger.selectAll((data) => {
-// //       handlebarsObject = {
-// //         burger: data
-// //       };
-// //       res.render("index", handlebarsObject);
-// //     });
-// // Index Page 
-// router.get('/index', function (req, res) 
-// {
-//   burger.selectAll(function(data) 
-//   {
-//     var hbsObject = { burgers: data };
-//     //console.log(hbsObject);
-//     res.render('index', hbsObject);
-//   });
-// });
-
-// // Create a New Burger
-// router.post('/api/burger', function (req, res) 
-// {
-//   burger.insertOne(req.body.burger_name, function() 
-//   {
-//     res.redirect('/index');
-//   });
-// });
-
-// // // POST Route
-// // router.post("/api/burger", function(req, res) {
-// //   burger.insertOne(["burger_name","devoured"], [req.body["burger_name"], req.body.devoured], (result)=>{
-// //     console.log(result);
-// //     res.json(result);
-// //   });
-// // });
-// // Devour a Burger
-// router.post('api/burger/:id', function (req, res) 
-// {
-//   burger.updateOne(req.params.id, function() 
-//   {
-//     res.redirect('/index');
-//   });
-// });
-
-// // router.put("/api/burger/:id", function(req, res) {
-
-// // Export routes
-// module.exports = router;
