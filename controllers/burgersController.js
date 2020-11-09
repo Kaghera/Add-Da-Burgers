576628973a8793fd5dd9de1e17fd220b1e7185e2
Kashyap Aghera <kaagh.10@gmail.com>
2020-11-08 23:00:34 -0500

    modified files

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -4,14 +4,27 @@ const router = express.Router();
 // Import burger.js
 const burger = require("../models/burgerModel.js");
 
-// GET Route
-router.get("/", function(req, res) {
+router.get('/', function (req, res) 
+{
+  res.redirect('/index');
+});
+router.get("/index", function(req, res) {
     burger.selectAll((data) => {
       handlebarsObject = {
         burger: data
       };
-      res.render("index", handlebarsObject);
+      res.render('index', handlebarsObject);
     });
+// Index Page 
+// router.get('/index', function (req, res) 
+// {
+//   burger.selectAll(function(data) 
+//   {
+//     var hbsObject = { burgers: data };
+//     //console.log(hbsObject);
+//     res.render('index', hbsObject);
+//   });
+// });
 
 });
 
@@ -20,6 +33,7 @@ router.post("/api/burger", function(req, res) {
   burger.insertOne(["burger_name","devoured"], [req.body["burger_name"], req.body.devoured], (result)=>{
     console.log(result);
     res.json(result);
+    //res.redirect('/index');
   });
 });
 
@@ -30,7 +44,68 @@ router.put("/api/burger/:id", function(req, res) {
 
   burger.updateOne(["devoured"], [req.body.devoured], condition, (result)=>{
     res.json(result);
+    //res.redirect('/index');
   });
 });
 
 module.exports = router;
+
+// var express = require('express');
+// var router = express.Router();
+// var burger = require('../models/burgerModel');
+
+
+// //Setup Routes
+
+// // Index Redirect
+// router.get('/', function (req, res) 
+// {
+//   res.redirect('/index');
+// });
+// // router.get("/", function(req, res) {
+// //     burger.selectAll((data) => {
+// //       handlebarsObject = {
+// //         burger: data
+// //       };
+// //       res.render("index", handlebarsObject);
+// //     });
+// // Index Page 
+// router.get('/index', function (req, res) 
+// {
+//   burger.selectAll(function(data) 
+//   {
+//     var hbsObject = { burgers: data };
+//     //console.log(hbsObject);
+//     res.render('index', hbsObject);
+//   });
+// });
+
+// // Create a New Burger
+// router.post('/api/burger', function (req, res) 
+// {
+//   burger.insertOne(req.body.burger_name, function() 
+//   {
+//     res.redirect('/index');
+//   });
+// });
+
+// // // POST Route
+// // router.post("/api/burger", function(req, res) {
+// //   burger.insertOne(["burger_name","devoured"], [req.body["burger_name"], req.body.devoured], (result)=>{
+// //     console.log(result);
+// //     res.json(result);
+// //   });
+// // });
+// // Devour a Burger
+// router.post('api/burger/:id', function (req, res) 
+// {
+//   burger.updateOne(req.params.id, function() 
+//   {
+//     res.redirect('/index');
+//   });
+// });
+
+// // router.put("/api/burger/:id", function(req, res) {
+
+// // Export routes
+// module.exports = router;
